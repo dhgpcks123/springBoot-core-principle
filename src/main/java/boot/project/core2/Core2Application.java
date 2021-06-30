5df529d2e2e565664ec265b5ba336eabbb079f9d
dhgpcks <dhgpcks123@naver.com>
2021-06-30 14:40:16 +0900

    git init

---
+++ src/main/java/boot/project/core2/Core2Application.java
@@ -0,0 +1,12 @@
+package boot.project.core2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Core2Application.class, args);
+    }
+}
