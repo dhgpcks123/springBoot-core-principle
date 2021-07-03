22e7d1f3ffa87ada01fb452f73e8e26f61870d23
dhgpcks <dhgpcks123@naver.com>
2021-07-03 15:16:39 +0900

    thymeleaf literal & operation

--- src/main/java/boot/project/core2/basic/BasicController.java
+++ src/main/java/boot/project/core2/basic/BasicController.java
@@ -69,6 +69,20 @@ public class BasicController {
         return "basic/link";
     }
 
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data","Spring!");
+        return "basic/operation";
+    }
+
+
 
     @Component("helloBean")
     static class HelloBean {
@@ -83,7 +97,5 @@ public class BasicController {
     static class User{
         private String username;
         private int age;
-
-
     }
 }
